9b62de7f5aaaa458f3df118ad503017097e3b9aa
MhMd099 <moalalhussein@gmail.com>
2026-04-26 13:48:27 +0200

    Übung 11: GitHub Actions Konfiguration fertiggestellt

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import os
+
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a,b):
+    return a-b
+
+if __name__ == "__main__":
+   
+    print("Ergebnis:", add_numbers(5, 3))
+    os.system("echo 'Check abgeschlossen'")
